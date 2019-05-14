131b106a269dd3537498ae1d766cda742801da94
bastimeyer <mail@bastimeyer.de>
2019-05-14 06:05:03 +0200

    native classes: link components
    also fix and add tests for DocumentationLinkComponent

--- src/app/ui/components/link/link-to/component.js
+++ src/app/ui/components/link/link-to/component.js
@@ -3,26 +3,23 @@ import LinkComponent from "@ember/routing/link-component";
 import { inject as service } from "@ember/service";
 
 
-export default LinkComponent.extend({
+export default class LinkToComponent extends LinkComponent {
 	/** @type {RouterService} */
-	router: service(),
+	@service router;
 
-	_active: computed(
-		"_routing.currentState",
-		"_routing.currentRouteName",
-		"attrs.params",
-		function() {
-			const currentState = this._routing.currentState;
+	@computed( "_routing.currentState", "_routing.currentRouteName", "attrs.params" )
+	get _active() {
+		const currentState = this._routing.currentState;
 
-			return currentState
-			    && this._isActive( currentState )
-			    && this._routing.currentRouteName !== "error";
-		}
-	),
+		return currentState
+		    && this._isActive( currentState )
+		    && this._routing.currentRouteName !== "error";
+	}
 
-	active: computed( "activeClass", "_active", "_routing.currentRouteName", function() {
+	@computed( "activeClass", "_active", "_routing.currentRouteName" )
+	get active() {
 		return this._active ? this.activeClass : false;
-	}),
+	}
 
 	/**
 	 * Checks whether the link is active because of an active child route.
@@ -46,8 +43,12 @@ export default LinkComponent.extend({
 		return routeInfosLeafName !== routeName
 		    && routeInfosLeafName !== `${routeName}.index`
 		    && routeInfosAncestors.find( ({ name }) => name === routeName );
-	},
+	}
 
+	/**
+	 * @param {MouseEvent} e
+	 * @returns {boolean}
+	 */
 	_invoke( e ) {
 		// prevent new windows from being opened
 		if ( e.buttons & 6 || e.shiftKey || e.ctrlKey || e.altKey || e.metaKey ) {
@@ -58,13 +59,14 @@ export default LinkComponent.extend({
 		// perform default action if link is inactive
 		// or if it is active, but only because of an active child route
 		if ( !this._active || this._isActiveAncestor() ) {
-			return this._super( ...arguments );
+			return super._invoke( ...arguments );
 		}
 
 		// enable route refreshing by clicking on the same link again
 		e.preventDefault();
 		e.stopImmediatePropagation();
 		this.router.refresh();
+
 		return false;
 	}
-});
+}
